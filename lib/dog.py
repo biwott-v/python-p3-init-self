bd42e881b26463e667c9874cafc26045171ab422
biwott-v <victor.biwott@student.moringaschool.com>
2025-06-06 11:25:53 +0300

    completes assignemtn

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self,name,breed="Mutt"):
+        self.breed=breed
+        self.name=name
+        print(f"print {name} is born")
+
+    def bark(self):
+        print("Woof!")
